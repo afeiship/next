bdf6e6df04d1d49ee9b3f9390190411aed7214fb
feizheng <1290657123@qq.com>
2016-09-23 10:20:15 +0800

    optimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
   var path = require('path');
   var gulp = require('gulp');
@@ -7,7 +7,6 @@
   var rename = require('gulp-rename');
   var uglify = require('gulp-uglify');
   var umd = require('gulp-umd');
-  var gulpFilter = require('gulp-filter');
 
   var conf = {
     src: 'src',
@@ -16,32 +15,22 @@
 
   var files = {
     src: [
-      conf.src + '/core/base.js',
-      conf.src + '/core/event.js',
-      conf.src + '/core/oop-base.js',
-      conf.src + '/core/oop-define-meta.js',
-      conf.src + '/core/oop.js',
-      conf.src + '/amd/Path.js',
-      conf.src + '/amd/Status.js',
-      conf.src + '/amd/Module.js',
-      conf.src + '/amd/Loader.js',
-      conf.src + '/amd/index.js'
+      'src/base.js',
+      'src/event.js',
+      'src/oop-base.js',
+      'src/oop-define-meta.js',
+      'src/oop.js'
     ],
-    dist: 'next-js-core2.js',
-    mini: 'next-js-core2.min.js'
+    dist: 'next-js-core2.js'
   };
 
-
-  var filter = gulpFilter(['*'], {restore: true});
-
-  gulp.task('clean', function () {
+  gulp.task('clean', function() {
     return del(conf.dist);
   });
 
-  gulp.task('uglify', ['clean'], function () {
+  gulp.task('scripts', ['clean'], function() {
     return gulp.src(files.src)
       .pipe(concat(files.dist))
-      .pipe(filter)
       .pipe(gulp.dest('dist'))
       .pipe(uglify())
       .pipe(rename({
@@ -50,22 +39,6 @@
       .pipe(gulp.dest('dist'));
   });
 
-  gulp.task('umd', ['uglify'], function () {
-    return gulp.src('dist/*.js')
-      .pipe(umd({
-        dependencies: function (file) {
-          return [];
-        },
-        exports: function (file) {
-          return 'nx';
-        },
-        namespace: function (file) {
-          return 'nx';
-        }
-      }))
-      .pipe(gulp.dest('dist'));
-  });
-
-  gulp.task('default', ['uglify']);
+  gulp.task('default', ['scripts']);
 
 }());
