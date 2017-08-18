9d02dfc993d91dbab9018530ed571a5d783d4e14
afeiship <1290657123@qq.com>
2017-08-18 10:30:06 +0800

    issue: add bump version support

---
+++ build/version.js
@@ -0,0 +1,36 @@
+(function() {
+
+    'use strict';
+
+    var gulp = require('gulp');
+    var config = require('./config');
+    var argv = require('yargs').argv;
+    var $ = require('gulp-load-plugins')({
+      pattern: ['gulp-*', 'gulp.*', 'del']
+    });
+
+    /**
+     * @thanks to:
+     * http://www.jianshu.com/p/d616d3bf391f
+     */
+
+    gulp.task('bump-json', function() {
+      gulp.src(['./*.json'])
+        .pipe($.bump())
+        .pipe(gulp.dest('./'));
+    });
+
+    gulp.task('bump-js',function(){
+      gulp.src(['./src/base.js'])
+      .pipe($.bump())
+      .pipe(gulp.dest('./src/'));
+    });
+
+    gulp.task('bump',function(){
+      gulp.start([
+        'bump-json',
+        'bump-js'
+      ]);
+    });
+
+  }());
