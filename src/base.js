d246e7446619ba80e6a0e077cdff4bbc17463d65
afeiship <afeiship@gmail.com>
2019-01-01 18:37:16 +0800

    feat: add set/get method for nx.path low layer

--- src/base.js
+++ src/base.js
@@ -2,13 +2,12 @@ nx = {
   BREAKER: {},
   VERSION: '__VERSION__',
   DEBUG: false,
-  GLOBAL: (function () {
+  GLOBAL: function() {
     return this;
-  }).call(null)
+  }.call(null)
 };
 
-(function (nx, global) {
-
+(function(nx, global) {
   var DOT = '.';
   var NUMBER = 'number';
   var ARRAY_PROTO = Array.prototype;
@@ -16,33 +15,19 @@ nx = {
   //global.nx will be 'undefined' in webpack/node env:
   global.nx = global.nx || nx;
 
-  nx.noop = function () {
-  };
+  nx.noop = function() {};
 
-  nx.error = function (inMsg) {
+  nx.error = function(inMsg) {
     throw new Error(inMsg);
   };
 
-  nx.forEach = function(inArray, inCallback, inContext){
+  nx.forEach = function(inArray, inCallback, inContext) {
     var length = inArray.length;
     var i;
     var result;
-    if( typeof length === NUMBER ){
+    if (typeof length === NUMBER) {
       for (i = 0; i < length; i++) {
-        result = inCallback.call(inContext, inArray[i], i, inArray );
-        if( result === nx.BREAKER){
-          break;
-        }
-      }
-    }
-  };
-
-  nx.forIn = function(inObject, inCallback, inContext){
-    var key;
-    var result;
-    for (key in inObject) {
-      if (inObject.hasOwnProperty(key)) {
-        result = inCallback.call(inContext, key, inObject[key], inObject );
+        result = inCallback.call(inContext, inArray[i], i, inArray);
         if (result === nx.BREAKER) {
           break;
         }
@@ -50,10 +35,25 @@ nx = {
     }
   };
 
-  nx.each = function (inTarget, inCallback, inContext) {
+  nx.forIn = function(inObject, inCallback, inContext) {
+    var key;
+    var result;
+    for (key in inObject) {
+      if (inObject.hasOwnProperty(key)) {
+        result = inCallback.call(inContext, key, inObject[key], inObject);
+        if (result === nx.BREAKER) {
+          break;
+        }
+      }
+    }
+  };
+
+  nx.each = function(inTarget, inCallback, inContext) {
     var key, length;
-    var iterator = function (inKey, inValue) {
-      return inCallback.call(inContext, inKey, inValue, inTarget) === nx.BREAKER;
+    var iterator = function(inKey, inValue) {
+      return (
+        inCallback.call(inContext, inKey, inValue, inTarget) === nx.BREAKER
+      );
     };
 
     if (inTarget) {
@@ -80,61 +80,74 @@ nx = {
     }
   };
 
-  nx.map = function(inTarget, inCallback, inContext){
-    var keys = typeof inTarget.length === NUMBER ? null : Object.keys( inTarget );
+  nx.map = function(inTarget, inCallback, inContext) {
+    var keys = typeof inTarget.length === NUMBER ? null : Object.keys(inTarget);
     var length = (keys || inTarget).length;
     var result = Array(length);
 
     for (var index = 0; index < length; index++) {
       var currentKey = keys ? keys[index] : index;
-      result[index] = inCallback.call(inContext, currentKey, inTarget[currentKey], inTarget );
+      result[index] = inCallback.call(
+        inContext,
+        currentKey,
+        inTarget[currentKey],
+        inTarget
+      );
     }
     return result;
   };
 
-  nx.mix = function (inTarget) {
+  nx.mix = function(inTarget) {
     var target = inTarget || {};
     var i, length;
     var args = arguments;
     for (i = 1, length = args.length; i < length; i++) {
-      nx.forIn(args[i], function (key, val) {
+      nx.forIn(args[i], function(key, val) {
         target[key] = val;
       });
     }
     return target;
   };
 
-  nx.slice = function (inTarget, inStart, inEnd) {
+  nx.slice = function(inTarget, inStart, inEnd) {
     return ARRAY_PROTO.slice.call(inTarget, inStart, inEnd);
   };
 
-  nx.path = function (inTarget, inPath, inValue) {
+  nx.set = function(inTarget, inPath, inValue) {
     var paths = inPath.split(DOT);
     var result = inTarget || nx.global;
     var last;
 
-    if (undefined === inValue) {
-      paths.forEach(function(path){
-        result = result && result[path];
-      })
-    } else {
-      last = paths.pop();
-      paths.forEach(function (path) {
-        result = result[path] = result[path] || {};
-      });
-      result[last] = inValue;
-    }
+    last = paths.pop();
+    paths.forEach(function(path) {
+      result = result[path] = result[path] || {};
+    });
+    result[last] = inValue;
     return result;
   };
 
-}(nx, nx.GLOBAL));
+  nx.get = function(inTarget, inPath) {
+    var paths = inPath.split(DOT);
+    var result = inTarget || nx.global;
 
+    paths.forEach(function(path) {
+      result = result && result[path];
+    });
+    return result;
+  };
+
+  nx.path = function(inTarget, inPath, inValue) {
+    return inValue == null
+      ? this.get(inTarget, inPath)
+      : this.set(inTarget, inPath, inValue);
+  };
+})(nx, nx.GLOBAL);
 
 if (typeof module !== 'undefined' && module.exports) {
   module.exports = nx;
 } else {
   if (typeof define === 'function' && define.amd) {
-    define([], function () {
+    define([], function() {
       return nx;
     });
   } else {
