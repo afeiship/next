56b647e204745995736ce4536600df361d3698c3
afeiship <afeiship@gmail.com>
2019-01-01 18:30:09 +0800

    style: format

--- test/base-path.js
+++ test/base-path.js
@@ -1,8 +1,7 @@
 var assert = require('assert');
 var nx = require('../src/base');
 
-describe('src/base-path', function () {
-
+describe('src/base-path', function() {
   // it('nx.path - get from object array[0]', function () {
   //   var object = { 'a': [{ 'b': { 'c1': 3, 'c2': 5 } }] };
   //   var object_copy = { 'a': [{ 'b': { 'c1': 3, 'c2': 5 } }] };
@@ -22,64 +21,52 @@ describe('src/base-path', function () {
 
   // });
 
-  it('nx.path - get undefined object',function(){
+  it('nx.path - get undefined object', function() {
     var object = {
       obj4: {
-        description: "toto"
+        description: 'toto'
       }
     };
 
-    var result = nx.path(object,'obj4.test.it');
-    assert.equal( result , undefined );
+    var result = nx.path(object, 'obj4.test.it');
+    assert.equal(result, undefined);
   });
 
-
-  it('nx.path - get from deep object', function () {
+  it('nx.path - get from deep object', function() {
     var object = {
-        obj1: {
-            obj2: {
-                data1: 213,
-                data2: "1231",
-                obj3: {
-                    data: "milf"
-                }
-            }
-        },
-        obj4: {
-            description: "toto"
+      obj1: {
+        obj2: {
+          data1: 213,
+          data2: '1231',
+          obj3: {
+            data: 'milf'
+          }
         }
+      },
+      obj4: {
+        description: 'toto'
+      }
     };
 
     var object_copy = {
       obj1: {
-          obj2: {
-              data1: 213,
-              data2: "1231",
-              obj3: {
-                  data: "milf"
-              }
+        obj2: {
+          data1: 213,
+          data2: '1231',
+          obj3: {
+            data: 'milf'
           }
+        }
       },
       obj4: {
-          description: "toto"
+        description: 'toto'
       }
-  };
+    };
 
+    assert.equal(nx.path(object, 'obj1.obj2.data1'), 213);
 
+    nx.path(object_copy, 'obj1.obj2.data1', 'afei');
 
-    assert.equal(
-      nx.path(object,'obj1.obj2.data1'), 213
-    );
-
-
-    nx.path(object_copy,'obj1.obj2.data1','afei');
-
-    assert.equal(
-      nx.path(object_copy,'obj1.obj2.data1'), 'afei'
-    );
-
-
+    assert.equal(nx.path(object_copy, 'obj1.obj2.data1'), 'afei');
   });
-
-
 });
