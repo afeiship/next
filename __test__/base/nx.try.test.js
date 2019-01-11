e8968c298600aa986a48b2d1e3393305dcf1ea09
afeiship <afeiship@gmail.com>
2019-01-11 10:05:32 +0800

    add: try unit

---
+++ __test__/base/nx.try.test.js
@@ -0,0 +1,20 @@
+require('../../src/base');
+
+var total = 0;
+function compileAndroidCode() {
+  nx.error('you are using the wrong nx.VERSION');
+  total++;
+}
+function compileAndroidCodeWithTry() {
+  nx.try(function() {
+    nx.error('you are using the wrong nx.VERSION');
+  });
+  total++;
+}
+
+test('compiling android goes as expected', () => {
+  expect(compileAndroidCode).toThrow();
+  expect(total).toBe(0);
+  compileAndroidCodeWithTry();
+  expect(total).toBe(1);
+});
