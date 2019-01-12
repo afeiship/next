09d3e4466c13e6802746374001f5361d89cc6221
afeiship <afeiship@gmail.com>
2019-01-12 14:46:09 +0800

    add: mix unit test

---
+++ __test__/base/nx.mix.test.js
@@ -0,0 +1,18 @@
+require('../../src/base');
+
+describe('src/base-mix', function() {
+  test('nx.mix', function() {
+    var obj1 = { name: 'fei' };
+    var obj2 = { email: '1290657123@qq.com' };
+
+    var result = {};
+
+    nx.mix(result, obj1, obj2);
+
+    expect(result.name).toBe(obj1.name);
+    expect(result.email).toBe(obj2.email);
+    expect(result).toEqual({ name: 'fei', email: '1290657123@qq.com' });
+    expect(obj1).toEqual({ name: 'fei' });
+    expect(obj2).toEqual({ email: '1290657123@qq.com' });
+  });
+});
