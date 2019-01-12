2a660332c5bc927596bed70eb50845ba4ecaee30
afeiship <afeiship@gmail.com>
2019-01-12 13:22:10 +0800

    add: forEach

--- __test__/base/nx.forEach.test.js
+++ __test__/base/nx.forEach.test.js
@@ -1,1 +1,21 @@
 require('../../src/base');
+
+describe('Array looping', () => {
+  var myArray;
+
+  beforeEach(function() {
+    myArray = ['A', 'B', 'C', 'D'];
+  });
+
+  test('should pass the index', function() {
+    nx.forEach(myArray, function(value, index) {
+      expect(String(index)).toMatch(/[0-9]/);
+    });
+  });
+
+  test('should pass the element', function() {
+    nx.forEach(myArray, function(value, index) {
+      expect(value).toMatch(/[A-D]/);
+    });
+  });
+});
