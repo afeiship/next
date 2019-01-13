b9097535a70ea96a2a41d59251d037a9cb6c8661
afeiship <afeiship@gmail.com>
2019-01-13 18:30:44 +0800

    opt: opmize class extends

--- __test__/oop/oop.es6.test.js
+++ __test__/oop/oop.es6.test.js
@@ -8,37 +8,38 @@ require('../../src/oop');
 */
 
 describe('oop class es6', () => {
-  test('Es6 class extends:', function() {
-    var num1 = 1;
-    var Class1 = nx.declare({
-      properties: {
-        prop1: {
-          get: function() {
-            return 'prop1';
-          }
-        },
-        prop2: 123,
-        prop3: null
-      },
-      methods: {
-        m1: function() {
-          num1++;
-        },
-        m3: function() {
-          return 10;
+  var num1 = 1;
+  var Class1;
+  Class1 = nx.declare({
+    properties: {
+      prop1: {
+        get: function() {
+          return 'prop1';
         }
-      }
-    });
-
-    class Class2 extends Class1 {
-      m2() {
-        num1 = num1 * 2;
-      }
-      m3() {
-        return super.m3() * 10;
+      },
+      prop2: 123,
+      prop3: null
+    },
+    methods: {
+      m1: function() {
+        num1++;
+      },
+      m3: function() {
+        return 10;
       }
     }
+  });
 
+  class Class2 extends Class1 {
+    m2() {
+      num1 = num1 * 2;
+    }
+    m3() {
+      return super.m3() * 10;
+    }
+  }
+
+  test('Es6 class extends:', function() {
     var cls2 = new Class2();
     cls2.m1();
     expect(num1).toBe(2);
@@ -52,4 +53,5 @@ describe('oop class es6', () => {
     cls2.prop1 = 123;
     expect(cls2.prop1).toBe('prop1');
   });
+
 });
